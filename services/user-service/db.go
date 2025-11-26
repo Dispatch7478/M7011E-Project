334f651ae0fe9ae4d94ffa601205968e6aac251d
rekhel-9 <rekhel-9@student.ltu.se>
2025-11-26 15:25:11 +0100

    hopefully fix for db

--- services/user-service/db.go
+++ services/user-service/db.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
@@ -11,7 +12,23 @@ import (
 func InitDB() *sql.DB {
 	dsn := os.Getenv("DB_DSN")
 	if dsn == "" {
-		log.Fatal("DB_DSN env variable is required")
+		host := os.Getenv("DB_HOST")
+		user := os.Getenv("DB_USERNAME")
+		pass := os.Getenv("DB_PASSWORD")
+		name := os.Getenv("DB_NAME")
+
+		if host == "" || user == "" || pass == "" || name == "" {
+			log.Fatal("database config missing: need DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME or DB_DSN")
+		}
+
+		dsn = fmt.Sprintf(
+			"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+			user,
+			pass,
+			host,
+			"5432",
+			name,
+		)
 	}
 
 	db, err := sql.Open("postgres", dsn)
