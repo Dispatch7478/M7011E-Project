68aa2e9524dfd0fb9104493b786b595c4eed993d
Dispatch <nahtse-2@student.ltu.se>
2025-11-27 16:33:08 +0100

    client

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,7 @@ import Keycloak from 'keycloak-js'
 const keycloak = new Keycloak({
   url: 'https://keycloak.ltu-m7011e-4.se',
   realm: 'master',
-  clientId: 'frontendtest-seb',
+  clientId: 't-hub-frontend',
 })
 
 const initOptions = {
